224426fdb48c5992558ffc67c1df26301c691ef3
sangsangeun <suev7245@gmail.com>
2017-07-18 16:33:31 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BaseActivity.setGlobalFont(this, getWindow().getDecorView());
 
         Button startButton = (Button)findViewById(R.id.login);
         startButton.setOnClickListener(new View.OnClickListener() {
